c1a87dd58b94f51535f4d4849926fbea0a2e3d81
Lindiks <lindsay.dickson@outlook.com>
2024-02-02 13:46:05 -0500

    Update- Request class
    Updates made to the constructor + addition of comments

--- src/Request.java
+++ src/Request.java
@@ -1,15 +1,28 @@
+/**
+ * Request class is responsible for providing the message structure and information of requests
+ * utilized by the synchronizer class
+ *
+ * @author Lindsay
+ * @date February 2nd, 2024
+ */
+
 public class Request {
     private String time;
     private int startFloor;
     private int destinationFloor;
     private String direction;
 
-    public Request(){ }
-    public Request(String time, String direction, int startFloor, int destinationFloor) {
+    public Request(){
+        time = null;
+        startFloor = -1;
+        destinationFloor = -1;
+        direction = null;
+    }
+    public Request(String time, int startFloor, int destinationFloor, String direction) {
         this.time = time;
-        this.direction = direction;
         this.startFloor = startFloor;
         this.destinationFloor = destinationFloor;
+        this.direction = direction;
     }
 
     public String getTime() { return time; }
