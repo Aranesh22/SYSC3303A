bfee649cc07ebca6865255db3ebbc6ced626cb22
Yehan De Silva <yehan@fiberdock.ca>
2024-02-02 10:31:11 -0500

    Request Boiler Plate Code

---
+++ Iteration1/src/Request.java
@@ -0,0 +1,26 @@
+public class Request {
+    private String time;
+    private int startFloor;
+    private int destinationFloor;
+    private String direction;
+
+    public Request() {
+
+    }
+
+    public String getTime() {
+        null;
+    }
+
+    public int getStartFloor() {
+        null;
+    }
+
+    public int getDestinationFloor() {
+        null;
+    }
+
+    public String getDirection() {
+        null;
+    }
+}
