974dcaa81e69573756acace05eebfe93ffd42848
Yehan De Silva <yehan@fiberdock.ca>
2024-02-02 10:21:36 -0500

    Elevator Boiler Plate Code

---
+++ Iteration1/src/Elevator.java
@@ -0,0 +1,26 @@
+public class Elevator extends Thread {
+
+    private Synchronizer synchronizer;
+    private int id;
+    private int curFloor;
+    private float velocity;
+    private float acceleration;
+    private float loadUnloadTime;
+
+    public Elevator() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public void goToFloor(int floor) {
+
+    }
+
+    public void loadUnloadElevator() {
+
+    }
+}
