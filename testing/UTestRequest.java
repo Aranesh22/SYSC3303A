4092532503410249143a204e4a174ba5c6202b50
Lindiks <lindsay.dickson@outlook.com>
2024-02-03 17:49:20 -0500

    Comments

--- testing/UTestRequest.java
+++ testing/UTestRequest.java
@@ -10,24 +10,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UTestRequest {
 
+    /**
+     * Testing Request Class method: getTime()
+     */
     @Test
     void test_getTime() {
         Request r = new Request("06:10:04:00", 1, 5, "up");
         assertEquals("06:10:04:00", r.getTime());
     }
 
+    /**
+     * Testing Request Class method: getStartFloor()
+     */
     @Test
     void test_getStartFloor() {
         Request r = new Request("06:10:04:00", 1, 5, "up");
         assertEquals(1, r.getStartFloor());
     }
 
+    /**
+     * Testing Request Class method: getDestinationFloor()
+     */
     @Test
     void test_getDestinationFloor() {
         Request r = new Request("06:10:04:00", 1, 5, "up");
         assertEquals(5, r.getDestinationFloor());
     }
 
+    /**
+     * Testing Request Class method: getDirection()
+     */
     @Test
     void test_getDirection() {
         Request r = new Request("06:10:04:00", 1, 5, "up");
